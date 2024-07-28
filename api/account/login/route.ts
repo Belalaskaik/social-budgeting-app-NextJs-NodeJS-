37e1a6333dc72eecc840a2b7fa7d43e54d06df9e
JasonChoyCode <86578475+JasonChoyCode@users.noreply.github.com>
2024-07-27 20:59:25 -0400

    change

--- api/account/login/route.ts
+++ api/account/login/route.ts
@@ -1,24 +1,24 @@
-import { cookies } from 'next/headers';
-import joi from 'joi';
+import { cookies } from "next/headers";
+import joi from "joi";
 
-import { usersRepo } from '_helpers/server';
-import { apiHandler } from '_helpers/server/api';
+import { usersRepo } from "../../../_helpers/server";
+import { apiHandler } from "../../../_helpers/server/api";
 
 module.exports = apiHandler({
-    POST: login
+  POST: login,
 });
 
 async function login(req: Request) {
-    const body = await req.json();
-    const { user, token } = await usersRepo.authenticate(body);
+  const body = await req.json();
+  const { user, token } = await usersRepo.authenticate(body);
 
-    // return jwt token in http only cookie
-    cookies().set('authorization', token, { httpOnly: true });
+  // return jwt token in http only cookie
+  cookies().set("authorization", token, { httpOnly: true });
 
-    return user;
+  return user;
 }
 
 login.schema = joi.object({
-    username: joi.string().required(),
-    password: joi.string().required()
+  username: joi.string().required(),
+  password: joi.string().required(),
 });
